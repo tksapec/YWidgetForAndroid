6b0199466c49d1f39623ebd77adb4dfca76291ed
tksapec <e1399068@gmail.com>
2026-08-23 09:00:25 +0900

    Complete enqueue transaction despite caller cancellation

--- app/src/main/java/com/tksapec/ywidget/work/RefreshEnqueueCoordinator.kt
+++ app/src/main/java/com/tksapec/ywidget/work/RefreshEnqueueCoordinator.kt
@@ -1,12 +1,18 @@
 package com.tksapec.ywidget.work
 
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 
 internal class RefreshEnqueueCoordinator {
     private val mutex = Mutex()
 
     suspend fun <T> runSerialized(block: suspend () -> T): T {
-        return mutex.withLock { block() }
+        return mutex.withLock {
+            withContext(NonCancellable) {
+                block()
+            }
+        }
     }
 }
